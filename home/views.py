a3f0c8188bc23e59befe3989a203c1d374658d85
Marcus <marcusfilipi.ventura@outlook.com>
2024-02-01 20:26:14 -0300

    Aula 427

--- home/views.py
+++ home/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
-from django.http import HttpResponse
 
-# from django.shortcuts import render
 
 
 def home(request):
     print('home')
-    return HttpResponse('home do app 1')
+    return render(request=request)
