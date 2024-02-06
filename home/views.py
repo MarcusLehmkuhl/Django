d9e344ce875856eb033e013ee3852c131d197c64
Marcus <marcusfilipi.ventura@outlook.com>
2024-02-06 17:20:27 -0300

    Aula 431

--- home/views.py
+++ home/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
 
 
-
-
 def home(request):
     print('home')
-    
+
+    context = {
+        'text': 'Olá home'
+    }
+
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context,
     )
